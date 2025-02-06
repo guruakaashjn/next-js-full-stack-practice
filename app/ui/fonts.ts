058593c85f0923ad621d818bcbb6728e8c5a4cb8
guruakaashjn <gurujnn@gmail.com>
2025-02-06 22:14:29 +0530

    add code files till chapter 6

---
+++ app/ui/fonts.ts
@@ -0,0 +1,11 @@
+// Used to keep the fonts that will be used throughout your application.
+
+import { Inter } from 'next/font/google';
+import { Lusitana } from 'next/font/google'
+
+export const inter = Inter({ subsets: ['latin'] })
+
+export const lusitana = Lusitana({ 
+    weight: ['400', '700'],
+    subsets: ['latin'] 
+});
